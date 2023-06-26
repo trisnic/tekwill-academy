be57b07d62cb7555faaedeba06e2d25da5c04893
trisnic <134542487+trisnic@users.noreply.github.com>
2023-06-26 21:56:41 +0300

    Update script.js
    Initial nu am inclus conditia "if" pentru a exclude "undefined", acum am inclus pentru "currencies" si pentru "capital" (acolo tot la mai multe tari lipsesc date). Insa nu mi se primeste sa pun aceasta conditie if pentru unul din 3 elemente din "currency" si anume "symbol" (el lipseste pentru 2 tari). Ma puteti ajuta cu aceasta?

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-
-  const createCountryContainer = (country) => {
+ const createCountryContainer = (country) => {
     const container = document.createElement('div');
     container.className = 'country-container';
     container.id = 'country-details';
@@ -17,18 +16,22 @@
     population.textContent = 'Population: ' + country.population;
     container.appendChild(population);
       
-    const currencies = document.createElement("h3");
-    currencies.textContent = 'Currency: ';
-    for (const currencyKey in country.currencies) {
-      const currency = country.currencies[currencyKey];
-      const currencyInfo = `${currencyKey} - ${currency.name} (${currency.symbol})`;
-      currencies.textContent += currencyInfo;
-    }
-    container.appendChild(currencies);
+    if (country.currencies) {
+        const currencies = document.createElement("h3");
+        currencies.textContent = 'Currency: ';
+        for (const currencyKey in country.currencies) {
+          const currency = country.currencies[currencyKey];
+          const currencyInfo = `${currencyKey} - ${currency.name} (${currency.symbol})`;
+          currencies.textContent += currencyInfo;
+        }
+        container.appendChild(currencies);
+      }
     
-    const capital = document.createElement("h3");
-    capital.textContent = 'Capital: ' + country.capital;
-    container.appendChild(capital);
+    if (country.capital) {
+      const capital = document.createElement("h3");
+      capital.textContent = 'Capital: ' + country.capital;
+      container.appendChild(capital);
+    }
     
     const continents = document.createElement("h3");
     continents.textContent = 'Continent: ' + country.continents;
