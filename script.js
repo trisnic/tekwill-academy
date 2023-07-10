cf86f182ed242d66835d26ccccfa214f65d4a02a
trisnic <134542487+trisnic@users.noreply.github.com>
2023-07-10 22:17:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,130 @@
- const createCountryContainer = (country) => {
-    const container = document.createElement('div');
-    container.className = 'country-container';
-    container.id = 'country-details';
+import { ENTER_KEY} from './search.js';
+import createCountryContainer from './createCountryContainer.js';
+// import { clonesDeep } from 'lodash';
 
-    const flagImg = document.createElement("img");
-    flagImg.src = country.flags.png;
-    flagImg.alt = country.flags.alt;
-    container.appendChild(flagImg);
+// const state = {
+//   shoppingCart: {product: 'bread', quantity: 5},
+// }
 
-    const name = document.createElement("h2");
-    name.textContent = 'Name: ' + country.name.common;
-    container.appendChild(name);
+// const copyState = Object.assign({}, state);
 
-    const population = document.createElement("h4");
-    population.textContent = 'Population: ' + country.population;
-    container.appendChild(population);
-      
-    if (country.currencies) {
-        const currencies = document.createElement("h3");
-        currencies.textContent = 'Currency: ';
-        for (const currencyKey in country.currencies) {
-          const currency = country.currencies[currencyKey];
-          const currencyInfo = `${currencyKey} - ${currency.name} (${currency.symbol})`;
-          currencies.textContent += currencyInfo;
+// const cloneDeepState = cloneDeep(state);
+
+// state.shoppingCart.quantity = 7;
+// console.log(copyState, cloneDeepState)
+
+// import oriceNumeDorit from 'module.js';
+
+
+
+
+
+let countriesList = [];
+
+const fetchData = () => {
+  fetch('https://restcountries.com/v3.1/all')
+  .then(response => response.json())
+  .then(countries => {
+    countriesList = countries;
+    countries.forEach(country => {
+      const countriesContainer = document.getElementById('countries');
+      const countryContainer = createCountryContainer(country);
+      countriesContainer.appendChild(countryContainer);
+    })
+  })
+  .catch(error => console.log(error));
+}
+
+fetchData();
+
+// Sort options
+const SortOptions = {
+  NAME: 'name',
+  POPULATION: 'population',
+  AREA: 'area',
+  CURRENCY: 'currency'
+};
+
+// Sort countries based on the selected parameter
+const sortCountries = (sortParam) => {
+  console.log(countriesList)
+  switch (sortParam) {
+    case SortOptions.POPULATION:
+      return countriesList.sort((a, b) => {
+        console.log(a);
+        return b.population - a.population}
+        );
+    case SortOptions.AREA:
+      return countriesList.sort((a, b) => b.area - a.area);
+    case SortOptions.CURRENCY:
+      return countriesList.sort((a, b) => {
+        if(a.currencies && b.currencies) {
+          const currencyCodeA = Object.keys(a.currencies)[0];
+          const currencyCodeB = Object.keys(b.currencies)[0];
+          return currencyCodeA.localeCompare(currencyCodeB);
         }
-        container.appendChild(currencies);
-      }
-    
-    if (country.capital) {
-      const capital = document.createElement("h3");
-      capital.textContent = 'Capital: ' + country.capital;
-      container.appendChild(capital);
+      });
+    case SortOptions.NAME:
+    default:
+      return countriesList.sort((a, b) => a.name.common.localeCompare(b.name.common));
+  }
+};
+
+// Function to update the countries container with sorted countries
+const updateCountries = (sortParam) => {
+  // if (typeof sortParam === String) {
+    const countriesContainer = document.getElementById('countries');
+    countriesContainer.innerHTML = ''; // Clear existing country containers
+  
+    const sortedCountries = sortCountries(sortParam);
+    sortedCountries.forEach(country => {
+      const countryContainer = createCountryContainer(country);
+      countriesContainer.appendChild(countryContainer);
+    });
+  // }
+};
+
+// Get sort select element and add event listener
+const sortSelect = document.getElementById('sort-select');
+sortSelect.addEventListener('change', () => {
+  const sortParam = sortSelect.value;
+  updateCountries(sortParam);
+});
+
+// sortSelect.addEventListener('keydown', (event) => {
+//   if (event.key === ENTER_KEY) {
+//     event.preventDefault();
+//     updateCountries(sortParam);
+//   }
+// });
+
+export const toggleMenu = () => {
+    const menuToggle = document.querySelector('.dropdown');
+    menuToggle.classList.toggle('closed');
+}
+
+const animateElement = (element, duration) => {
+  let position = 0;
+  // distanta totala de miscare in pixeli
+  const distance = 100;
+  const step = distance / duration; // 10px
+
+  // 0 10 20 30 ... 100
+  const move = () => {
+    position += step;
+    element.style.transform = `translateX(${position}px)`;
+    if (position < distance) {
+      setTimeout(move, 10);
     }
-    
-    const continents = document.createElement("h3");
-    continents.textContent = 'Continent: ' + country.continents;
-    container.appendChild(continents);
-
-    return container;
   }
 
-  const fetchData = () => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(response => response.json())
-      .then(countries => {
-        countries.forEach(country => {
-          const countriesContainer = document.getElementById('countries');
-          const countryContainer = createCountryContainer(country);
-          countriesContainer.appendChild(countryContainer);
-        })
-      })
-      .catch(error => console.log(error));
-  }
+  move();
+};
 
-  fetchData();
+// window.addEventListener('load', () => {
+//   const image = document.querySelector('.country-container img');
+//   animateElement(image, 100);
+// })
+
+window.toggleMenu=toggleMenu;
+window.fetchData=fetchData;
